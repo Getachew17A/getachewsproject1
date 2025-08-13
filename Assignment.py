380c51a254d2939c08d665e92e1a5cd47403fcc0
Getachew17A <getachewambachew17@gmail.com>
2025-08-14 01:10:47 +0300

    changes

--- Assignment.py
+++ Assignment.py
@@ -1,1 +1,30 @@
+# Ask the user for two numbers
+x = float(input("Enter the first number to add:"))
+y = float(input("enter the second number to add"))
+
+# Ask the user for the operation
+Operation = input("Enter an operation(+, -, *, /): ")
+
+# perform the operation and print the result
+if Operation == '+':
+    result = x + y
+    print(f"{x} + {y} = {result}")
+elif Operation == '-':
+    result = x - y
+    print(f"{x} - {y} = {result}")
+elif Operation == '*':
+    result = x * y
+    print(f"{x} * {y} = {result}")
+elif Operation == '/':
+    if y !=0:
+        result = x / y
+        print(f"{x} / {y} = {result}")
+    else:
+        print("Error: Division by Zero is not allowed")
+        
+else:
+    print("invalid operation. Please Enter one of +, -, *, /.")
+    
+            
+
 print("this is my first code")
